44e533004f051a27f66965fba5a766ef89fd3da8
Alafe <alaferodrigo18@gmail.com>
2020-09-01 15:50:46 -0300

    aula 03 feita

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # PHP Orientado a Objeto
 
 01 - Criando Classes e Objetos em PHP
+
+02 - Configurando Visibilidade de Atributos e Métodos
+
+03 - Métodos Getter, Setter e Construtor
---
+++ aula03/Caneta.php
@@ -0,0 +1,29 @@
+<?php
+class Caneta
+{
+	private $modelo;
+	private $ponta;
+	private $cor;
+
+	public function __construct($m, $c, $p){
+		$this->modelo = $m;
+		$this->cor = $c;
+		$this->ponta = $p;
+	}
+
+	public function getModelo(){
+		return $this->modelo;
+	}
+
+	public function getPonta(){
+		return $this->ponta;
+	}
+
+	public function setModelo($m){
+		$this->modelo = $m;
+	}
+
+	public function setPonta($p){
+		$this->ponta = $p;
+	}
+}
